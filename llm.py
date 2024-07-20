daf70e2587304b26daaa442cbf3594a935ff66b1
Alp Akpinar <aakpinar@bu.edu>
2024-07-20 11:35:50 -0400

    Commit completed version

--- llm.py
+++ llm.py
@@ -1,6 +1,14 @@
 import streamlit as st
 
 # Create the LLM
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
+llm = ChatOpenAI(
+    openai_api_key=st.secrets["OPENAI_API_KEY"],
+    model=st.secrets["OPENAI_MODEL"],
+)
 
 # Create the Embedding model
+embeddings = OpenAIEmbeddings(
+    openai_api_key=st.secrets["OPENAI_API_KEY"]
+)
